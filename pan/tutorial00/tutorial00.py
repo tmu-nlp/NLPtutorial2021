f3e3ed672a36df60c3441c173e0cdecb793ffafb
pxm427 <78017600+pxm427@users.noreply.github.com>
2021-04-30 14:37:19 +0900

    Update tutorial00.py

--- pan/tutorial00/tutorial00.py
+++ pan/tutorial00/tutorial00.py
@@ -1,14 +1,34 @@
+import sys
+
+counts = {}
+file = open(sys.argv[1])
+
+words = []
+for line in file:  
+  words = line.split()
+    
+  for word in words:
+      if word in counts:
+          counts[word] +=1
+      else:
+          counts[word] = 1
+
+for x, y in sorted(counts.items()):
+  print ('%s: %r' % (x, y))
+
+print('\nNumber of Unique Words: ' + str(len(counts)))
 
 
-def wordcount(text):
-  list = text.split()
-  dict = {}
-  for word in list:
-    if word in dict:
-      dict[word] += 1
-    else:
-      dict[word[ = 1
-  return dict
+
+#def wordcount(text):
+#  list = text.split()
+#  dict = {}
+#  for word in list:
+#    if word in dict:
+#      dict[word] += 1
+#    else:
+#      dict[word[ = 1
+#  return dict
                 
                 
                 
