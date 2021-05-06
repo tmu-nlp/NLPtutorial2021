d76754ee85b56a82dbe108fdd08283d8ca9ab63a
Ling zhidong <note980869266@gmail.com>
2021-05-06 12:07:26 +0900

    added bigram(functional) and ngram

---
+++ ling/tutorial02/test-ngram.py
@@ -0,0 +1,34 @@
+import sys,math
+
+prob=dict()#確率を保存する辞書
+# パラメータ
+l1=0.95
+l2=0.999
+V=1000000.0 #未知語を含む語彙数
+W=0
+H=0.0
+unk=0
+
+read_model=open("./model_file_ngram.word","r",encoding='utf-8')#モデルを読み込む
+for line in read_model:
+    line=line.strip()
+    line=line.split(" ")
+    prob[" ".join(line[:-2])]=line[-1]
+read_model.close()
+
+test_file=open(r"C:\Users\Lexus\Documents\GitHub\Nlptutorial\data\wiki-en-test.word","r",encoding='utf-8')
+for line in test_file:
+    sentence=line.strip().split(" ")
+    sentence.append("</s>")
+    sentence.insert(0,"<s>")
+    for i in range(1,len(sentence)-1):
+        word=sentence[i-1]+" "+sentence[i]
+        if word in prob:
+            p1=l1*float(prob[sentence[i]])+(1.0-l1)/V
+            p2=l2*float(prob[word])+(1.0-l2)*p1
+            H+=-math.log2(p2)
+            W+=1
+        else:
+            unk+=1
+print("entropy= "+ str(+H/W))
+    
---
+++ ling/tutorial02/train-ngram.py
@@ -0,0 +1,42 @@
+import sys
+n=int(input("input n(n<=4): "))
+
+training_file=open(r"C:\Users\Lexus\Documents\GitHub\Nlptutorial\data\wiki-en-train.word","r",encoding='utf-8')
+
+def train(training_file,n):
+    counts=dict()
+    context_counts=dict()
+    context_counts[""]=0
+    for line in training_file:
+        line=line.strip()
+        if len(line)!=0:
+            sentence=line.split(" ")#スペースで単語を分ける
+            sentence.append("</s>")#文末記号
+            sentence.insert(0,"<s>")#文頭記号
+            for i in range(n-1,len(sentence)-1):
+                word=" ".join(sentence[i-n+1:i+1])#target word bound with context words
+                context_word=" ".join(sentence[i-n+1:i])#only context words 
+                if word in counts:
+                    counts[word]+=1
+                else:
+                    counts[word]=1
+
+                if context_word in context_counts:
+                    context_counts[context_word]+=1
+                else:
+                    context_counts[context_word]=1
+        
+    training_file.close()
+    ans=open("model_file_ngram.word","w",encoding='utf-8')
+    for ngram in counts:
+        tmp=ngram.split(" ")
+        tmp.pop(-1)
+        context=" ".join(tmp)
+        #print(context)
+        prob=float(counts[ngram]/context_counts[context])
+        ans.write(ngram+" "+str(prob)+"\n")
+    ans.close()
+    print("training finished")
+
+train(training_file,n)
+
