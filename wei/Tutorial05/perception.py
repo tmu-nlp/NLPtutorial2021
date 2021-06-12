8391edbad2843e21bcd0f73eb4f78342ee15a0f4
houjing.wei <weihoujing@163.com>
2021-06-13 00:42:03 +0800

    fixed tutorial05,and create tutorial07

--- wei/Tutorial05/perception.py
+++ wei/Tutorial05/perception.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
 
-
+# 1つの事例に対する予測, w-> 重みで、defaultdict(int), phi -> 各k:v(素性:重み)の辞書
 def predict_one(w, phi):
-    score = 0
-    for k,v in phi.items():
+    score = 0                   # score = w*φ(x)
+    for k,v in phi.items():     # k->'UNI:word';v->counts≒weights
         if k in w:
             score += v * w[k]
     if score >= 0:
@@ -11,20 +11,25 @@ def predict_one(w, phi):
     else:
         return -1
 
-def create_features(x):
+# 1―gram素性を作成
+def create_features(x):             # xは素性リストで、各素性に重みを割り振る
     phi = defaultdict(int)
     for word in x:
         phi['UNI:' + word] += 1
     return phi
+    # defaultdict(int, {'UNI:word': counts,...})を返す
 
-
+    # 重みを更新: w <- w + yφ(x), y -> label
+    # y=1の場合、φ(x)の素性の重みを増やす≒「yes」の事例の素性により大きな重み
+    # そうでないと、より小さな重みを
 def update_weight(w, phi, y):
     for k, v in phi.items():
         w[k] += v * y
 
+# 予測
 def predict_all(model_file, input_file):
     weights = defaultdict(int)
-
+    #　modelfile、即ちオンライン学習されて出したdatafileを読み込む
     with open(model_file, 'r', encoding='utf-8') as f:
         for line in f.readlines():
             line = line.strip().split()
@@ -32,7 +37,8 @@ def predict_all(model_file, input_file):
             feature = line[0]
             weight = line[1]
             weights[feature] = int(weight)
-            #print(weights.items())
+    print(weights)
+    #　testfileを読み込み、
     with open(input_file, 'r', encoding= 'utf-8') as filein, \
         open('./my_answer.txt', 'w', encoding= ' utf-8') as fileout:
         for line in filein.readlines():
@@ -43,7 +49,7 @@ def predict_all(model_file, input_file):
 
 
 class MyPerceptron:
-
+    # ラベル付きデータからオンライン学習を行い、予測結果次第で重みを更新。
     def train_perceptron(self, model_file, output_file, num_iterations: int):
         '''
         model_file = 'titles-en-train.labeled'
@@ -58,12 +64,13 @@ class MyPerceptron:
                     label = int(line[0])
                     phi = create_features(features)
                     print(phi)
-                    y_predict = predict_one(weight, phi)
+                    y_predict = predict_one(weight, phi)   # 各学習事例を予測して分類
 
-                    if y_predict != label:
+                    if y_predict != label:      # 間違った答えの時、重みを更新
                         update_weight(weight, phi, label)
-
-        with open(output_file, 'w', encoding= 'utf-8') as answer:
+        print(weight)
+        # その後、更新した各k:v(素性:重み)をanswer.txtに保存
+        with open(output_file, 'w', encoding='utf-8') as answer:
             for k,v in weight.items():
                 answer.write(k + '\t' + str(v) + '\n')
 
---
+++ wei/Tutorial07/train_nn.py
@@ -0,0 +1,172 @@
+from collections import defaultdict
+import numpy as np
+from tqdm import tqdm
+from typing import List
+from sklearn.metrics import classification_report, accuracy_score
+# 学習1回、隠れ層1つ、隠れ層のノード2つ
+# 中国語の備考は自分の理解のため
+
+
+class NeuralNet:
+    def __init__(self, layer_num, node_num):
+        self.L = layer_num
+        self.N = node_num
+        # 素性のID化(素性を整数IDに変換する)。default_factory为可调用的匿名表达式，
+        # 每遇到新的key，就将对应的value初始化为当前字典的元素个数，然后把构建好的键值对添加进字典.
+        # 添加k-v时，len(dict)仍为 上一次添加后的长度。故第一次添加时，其value为0
+        #　defaultdict(<function__main__.<lambda>()>, {'key':len(ids),...})
+        self.ids = defaultdict(lambda: len(self.ids))
+        self.features = []
+        self.net = []
+
+    def predict_one(self, phi0):
+        phis = [phi0]
+        for i in range(len(self.net)):
+            w, b = self.net[i]
+            phis.append(np.tanh(np.dot(w, phis[i]) + b))
+        return phis[len(self.net)][0]
+
+    # 1―gram素性を整数IDに変換≒各素性に重みを割り振る
+    def create_features(self, sentence, is_train=True):
+        phi = [0 for _ in range(len(self.ids))]     # 0要素のリスト
+        if is_train:
+            for word in sentence.split():
+                key = 'UNI:' + word
+                phi[self.ids[key]] += 1             # ラベル付きデータの各素性に対応する重みを取得
+        else:
+            for word in sentence.split():
+                key = 'UNI:' + word
+                if key in self.ids:
+                    phi[self.ids[key]] += 1
+        #print(phi)
+        return phi
+
+
+    def prepare(self, input_file):
+        input_data = []
+        # ラベル付きファイルを読み込み、[句,ラベル]のリストを作成
+        with open(input_file, 'r', encoding='utf-8') as infile:
+            for line in infile:
+                label, sentence = line.rstrip().split('\t')
+                for word in sentence.split():
+                    self.ids['UNI:' + word]
+                    # 将'UNI:' + word 作为key追加到ids字典，value为len(ids),
+                    # 第一个ids[key]的value为0
+                input_data.append([sentence, int(label)])
+        # print(input_data)
+        #　[[[ラベル付きデータの各素性に対応する重み],ラベル]]の素性リストを作成
+        for sentence, label in input_data:
+            self.features.append([self.create_features(sentence), label])
+
+
+    # 素性の初期化
+    def init_net(self):
+        # np.random.rand(n, m): 0.0以上, 1.0未満の 一様分布のn行m列，行ごとにm列
+        # 返回一个或一组服从“0-1”均匀分布的随机样本值，r.v.取值范围为[0,1)，无参数时返回foalt；
+        # 在DL中，可用于生成dropout随机向量
+        # 入力層，重みは－0.1以上0.1未満でランダムで初期化
+        w_in = np.random.rand(self.N, len(self.ids)) / 5 - 0.1
+        b_in = np.random.rand(self.N) / 5 - 0.1
+        self.net.append((w_in, b_in))
+
+        # 隠れ層
+        for _ in range(self.L-1):
+            w = np.random.rand(self.N, self.N)/5 - 0.1
+            b = np.random.rand(self.N)/5 - 0.1
+            self.net.append((w, b))
+
+        # 出力層
+        w_out = np.random.rand(1, self.N)/5 - 0.1
+        b_out = np.random.rand(1)/5 - 0.1
+        self.net.append((w_out, b_out))
+
+
+    # def predict_all(self, test_file):
+    #     with open('./my_answer.txt', 'w', encoding='utf-8') as outfile:
+    #         with open(test_file, 'r', encoding='utf-8') as infile:
+    #             for sentences in infile:
+    #                 sentence = sentences.strip()
+    #                 phi = self.create_features(sentence, False)
+    #                 y_d = self.predict_one(self.net, phi)
+    #                 outfile.write(str(y_d) + '\t' + sentence +'\n')
+
+    def forward_nn(self, phi_0):
+        #　各層の値
+        phi = [0 for _ in range(len(self.net)+1)]
+        phi[0] = phi_0
+        for i in range(len(self.net)):
+            w, b =self.net[i]
+            #　前の層の値に基づいて値を計算
+            phi[i+1] = np.tanh(np.dot(w, phi[i]) + b)
+            #print(phi)
+        #　各層の結果を返す
+        return phi
+
+    def backward_nn(self, phi, y_d):        # y_dは正解
+        J = len(self.net)
+        delta = [0 for _ in range(J)]
+        delta.append(y_d - phi[J])
+        delta_d = [0 for _ in range(J+1)]
+        for i in reversed(range(J)):
+            delta_d[i+1] = delta[i+1] * (1 - phi[i+1] ** 2)
+            w, b = self.net[i]
+            delta[i] = np.dot(delta_d[i+1], w)
+        return delta_d
+    # 重みの勾配を計算して、更新
+    def update_weights(self, phi, delta_d, lamb):
+        for i in range(len(self.net)):
+            w, b = self.net[i]
+            w += lamb * np.outer(delta_d[i+1], phi[i])
+            b += lamb * delta_d[i+1]
+
+    # 学習を行う
+    def train_nn(self, num_iter,lamb):
+        for i in tqdm(range(num_iter)):
+            for phi_0, y in tqdm(self.features):
+                phi = self.forward_nn(phi_0)
+                delta_d = self.backward_nn(phi, y)
+                self.update_weights(phi, delta_d, lamb)
+        return self.net, self.ids
+
+    def test_nn(self, sentence):
+        phi0 = self.create_features(sentence.rstrip(), False)
+        score = self.predict_one(phi0)
+        return 1 if score >0 else -1
+
+def check_score(gold_file:str, pred:List[int], detail:bool = False):
+    gold = []
+    with open(gold_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            label = int(line.split('\t')[0])
+            gold.append(label)
+    gold = np.array(gold)
+    pred = np.array(pred)
+    if detail:
+        print(classification_report(gold, pred))
+    print(f'accuracy:{accuracy_score(gold, pred)}')
+
+
+if __name__ == '__main__':
+    input_file = '../data/titles-en-train.labeled'
+    test_file = '../data/titles-en-test.word'
+    test_labeled = '../data/titles-en-test.labeled'
+
+    nn = NeuralNet(1, 2)
+    print('Loading data...')
+    nn.prepare(input_file)
+    nn.init_net()
+    print('Training...')
+    nn.train_nn(1, 0.1)
+    print('Predicting...')
+    ans = []
+    for sentence in open(test_file, 'r', encoding='utf-8'):
+        pred = nn.test_nn(sentence)
+        ans.append(pred)
+    check_score(test_labeled, ans, True)
+
+
+
+
+
+
+
